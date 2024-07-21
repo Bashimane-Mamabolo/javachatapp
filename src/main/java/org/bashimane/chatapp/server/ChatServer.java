da7a04dbd9239045e2dd42869cc6ba7fcf67ea1f
Bashimane-Mamabolo <mamabolobashimane@gmail.com>
2024-07-21 13:12:11 +0200

    Added logging

--- src/main/java/org/bashimane/chatapp/server/ChatServer.java
+++ src/main/java/org/bashimane/chatapp/server/ChatServer.java
@@ -4,24 +4,29 @@ import java.io.*;
 import java.net.*;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ChatServer {
 
+    private static final Logger logger = Logger.getLogger(ChatServer.class.getName());
     private static final List<ClientChatHandler> clients = new CopyOnWriteArrayList<>();
 
     public static void main(String[] args) throws IOException {
         int portNumber = 6888;
         try (ServerSocket serverSocket = new ServerSocket(portNumber)) {  // Port number 68888
-            System.out.println("Server started. Waiting for clients...");
+            logger.info("Server started. Waiting for clients...");
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                System.out.println("Client connected: " + clientSocket);
+                logger.info("Client connected: " + clientSocket);
 
                 ClientChatHandler clientHandler = new ClientChatHandler(clientSocket, clients);
                 clients.add(clientHandler);
                 Thread clientThread = new Thread(clientHandler);
                 clientThread.start();
             }
+        } catch (IOException ex){
+            logger.log(Level.SEVERE, "An error occurred while starting the server: " + ex.getMessage(), ex);
         }
     }
 }
