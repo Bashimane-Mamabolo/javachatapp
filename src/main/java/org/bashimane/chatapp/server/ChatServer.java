755001f0bc139ebb2afe7164042a81d6f25db9eb
Bashimane-Mamabolo <mamabolobashimane@gmail.com>
2024-07-21 13:09:26 +0200

    refactored ChatServer class

--- src/main/java/org/bashimane/chatapp/server/ChatServer.java
+++ src/main/java/org/bashimane/chatapp/server/ChatServer.java
@@ -2,24 +2,25 @@ package org.bashimane.chatapp.server;
 
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ChatServer {
 
-    private static final List<ClientChatHandler> clients = new ArrayList<>();
+    private static final List<ClientChatHandler> clients = new CopyOnWriteArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        try (ServerSocket serverSocket = new ServerSocket(6888)) {  // Port number 68888
+        int portNumber = 6888;
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {  // Port number 68888
             System.out.println("Server started. Waiting for clients...");
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Client connected: " + clientSocket);
 
-                ClientChatHandler clientThread = new ClientChatHandler(clientSocket, clients);
-                clients.add(clientThread);
-                Thread task = new Thread(clientThread);
-                task.start();
+                ClientChatHandler clientHandler = new ClientChatHandler(clientSocket, clients);
+                clients.add(clientHandler);
+                Thread clientThread = new Thread(clientHandler);
+                clientThread.start();
             }
         }
     }
